0b2d2d9ff76155d1b73adc53a7bef61b9b5c94cd
planit <planit@tanjeffreyz.github.io>
2023-10-18 06:36:17 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -168,6 +168,13 @@ assignments = {
       "dueDate": "2023-10-25T23:59:00",
       "link": "https://www.gradescope.com/courses/637026",
       "submitted": false
+    },
+    {
+      "title": "Written Homework #1 resubmission ",
+      "course": "PHYS2C_FA23_A00",
+      "dueDate": "2023-10-25T23:59:00",
+      "link": "https://www.gradescope.com/courses/637026",
+      "submitted": false
     }
   ]
 };
