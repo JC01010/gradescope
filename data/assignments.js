5ea413198696c81300fa50d763db173e592831a0
planit <planit@tanjeffreyz.github.io>
2023-11-01 04:34:16 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -95,7 +95,7 @@ assignments = {
       "title": "HW4 (Coding): Life",
       "course": "CSE30_FA23_A00",
       "dueDate": "2023-10-31T23:59:00",
-      "link": "https://www.gradescope.com/courses/630948/assignments/3573476/submissions/208236965",
+      "link": "https://www.gradescope.com/courses/630948/assignments/3573476/submissions/208258313",
       "submitted": true
     },
     {
