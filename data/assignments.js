cf906720a7ef944791b47482b0bf826f66b4990d
planit <planit@tanjeffreyz.github.io>
2023-11-10 20:34:33 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -250,6 +250,13 @@ assignments = {
       "dueDate": "2023-11-08T23:59:00",
       "link": "https://www.gradescope.com/courses/641084",
       "submitted": false
+    },
+    {
+      "title": "Homework 5",
+      "course": "MATH184_FA23_A00",
+      "dueDate": "2023-11-15T23:59:00",
+      "link": "https://www.gradescope.com/courses/641084",
+      "submitted": false
     }
   ],
   "PHYS2C_FA23_A00": [
