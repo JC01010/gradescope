02d746bd353038aed755bc0ba23dd2a1877ce703
planit <planit@tanjeffreyz.github.io>
2023-11-11 17:34:26 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -146,6 +146,13 @@ assignments = {
       "dueDate": "2023-11-07T23:59:00",
       "link": "https://www.gradescope.com/courses/630948/assignments/3635328/submissions/210577580",
       "submitted": true
+    },
+    {
+      "title": "HW6 (Coding): Conversions",
+      "course": "CSE30_FA23_A00",
+      "dueDate": "2023-11-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/630948",
+      "submitted": false
     }
   ],
   "MAE21_FA23_A00": [
