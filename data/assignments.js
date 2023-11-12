54e447b2c27ae280aa84cdfd3e82e7b21ce22554
planit <planit@tanjeffreyz.github.io>
2023-11-12 01:38:22 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -153,6 +153,13 @@ assignments = {
       "dueDate": "2023-11-14T23:59:00",
       "link": "https://www.gradescope.com/courses/630948",
       "submitted": false
+    },
+    {
+      "title": "HW5 (Coding): Transaction Lookup [RESUBMISSION]",
+      "course": "CSE30_FA23_A00",
+      "dueDate": "2023-11-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/630948",
+      "submitted": false
     }
   ],
   "MAE21_FA23_A00": [
