39071faa02881a41e6237873f64af1d719b5dd73
planit <planit@tanjeffreyz.github.io>
2023-11-26 09:34:08 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -171,7 +171,7 @@ assignments = {
     {
       "title": "HW7 (Coding): Part 1 ASM and Part 2",
       "course": "CSE30_FA23_A00",
-      "dueDate": "2023-11-28T11:59:00",
+      "dueDate": "2023-11-28T23:59:00",
       "link": "https://www.gradescope.com/courses/630948",
       "submitted": false
     },
