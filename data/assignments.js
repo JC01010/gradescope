36bbed966e001e40a018d062a8ebea94d2d26bfc
planit <planit@tanjeffreyz.github.io>
2024-01-04 01:35:46 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,34 +1,3 @@
 assignments = {
-  "CSE105_WI24_A00": [
-    {
-      "title": "Week 1 Monday Review Quiz",
-      "course": "CSE105_WI24_A00",
-      "dueDate": "2024-01-12T17:00:00",
-      "link": "https://www.gradescope.com/courses/688881",
-      "submitted": false
-    },
-    {
-      "title": "Week 1 Friday Review Quiz",
-      "course": "CSE105_WI24_A00",
-      "dueDate": "2024-01-12T17:00:00",
-      "link": "https://www.gradescope.com/courses/688881",
-      "submitted": false
-    },
-    {
-      "title": "Week 1 Wednesday Review Quiz",
-      "course": "CSE105_WI24_A00",
-      "dueDate": "2024-01-12T17:00:00",
-      "link": "https://www.gradescope.com/courses/688881",
-      "submitted": false
-    }
-  ],
-  "CSE95_WI24_A00": [
-    {
-      "title": "Week 1 before class: Getting started, asking for help, and the imposter phenomenon",
-      "course": "CSE95_WI24_A00",
-      "dueDate": "2024-01-10T08:00:00",
-      "link": "https://www.gradescope.com/courses/682820/assignments/3801530/submissions/new",
-      "submitted": false
-    }
-  ]
+  "CSE101_WI24_A00": []
 };
