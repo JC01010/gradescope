0b2a3f29302f3efe82fc4335c7aae892046823f6
planit <planit@tanjeffreyz.github.io>
2024-01-08 20:34:50 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,3 +1,126 @@
 assignments = {
-  "CSE101_WI24_A00": []
+  "CSE101_WI24_A00": [],
+  "CSE105_WI24_A00": [
+    {
+      "title": "Second practice assignment (Makeup submission for another HW assignment)",
+      "course": "CSE105_WI24_A00",
+      "dueDate": "2024-03-15T17:00:00",
+      "link": "https://www.gradescope.com/courses/688881",
+      "submitted": false
+    },
+    {
+      "title": "Practice assignment (Makeup submission for HW)",
+      "course": "CSE105_WI24_A00",
+      "dueDate": "2024-03-15T17:00:00",
+      "link": "https://www.gradescope.com/courses/688881",
+      "submitted": false
+    },
+    {
+      "title": "Week 2 Friday Review Quiz",
+      "course": "CSE105_WI24_A00",
+      "dueDate": "2024-01-19T17:00:00",
+      "link": "https://www.gradescope.com/courses/688881",
+      "submitted": false
+    },
+    {
+      "title": "Week 1 Monday Review Quiz",
+      "course": "CSE105_WI24_A00",
+      "dueDate": "2024-01-12T17:00:00",
+      "link": "https://www.gradescope.com/courses/688881/assignments/3836721/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Week 1 Friday Review Quiz",
+      "course": "CSE105_WI24_A00",
+      "dueDate": "2024-01-12T17:00:00",
+      "link": "https://www.gradescope.com/courses/688881/assignments/3836724/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Week 1 Wednesday Review Quiz",
+      "course": "CSE105_WI24_A00",
+      "dueDate": "2024-01-12T17:00:00",
+      "link": "https://www.gradescope.com/courses/688881/assignments/3836725/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Week 2 Wednesday Review Quiz",
+      "course": "CSE105_WI24_A00",
+      "dueDate": "2024-01-19T17:00:00",
+      "link": "https://www.gradescope.com/courses/688881",
+      "submitted": false
+    }
+  ],
+  "CSE95_WI24_A00": [
+    {
+      "title": "Week 1 before class: Getting started, asking for help, and the imposter phenomenon",
+      "course": "CSE95_WI24_A00",
+      "dueDate": "2024-01-10T08:00:00",
+      "link": "https://www.gradescope.com/courses/682820/assignments/3801530/submissions/221567901",
+      "submitted": true
+    },
+    {
+      "title": "Week 2 before class: Experienced Tutor Panel",
+      "course": "CSE95_WI24_A00",
+      "dueDate": "2024-01-17T08:00:00",
+      "link": "https://www.gradescope.com/courses/682820/assignments/3801532/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Week 3 before class: Grading and giving feedback: rubrics and reverse engineering student mistakes",
+      "course": "CSE95_WI24_A00",
+      "dueDate": "2024-01-24T08:00:00",
+      "link": "https://www.gradescope.com/courses/682820/assignments/3801533/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Week 5 before class: Tutoring demo roleplay: meeting students where they are",
+      "course": "CSE95_WI24_A00",
+      "dueDate": "2024-02-07T08:00:00",
+      "link": "https://www.gradescope.com/courses/682820/assignments/3801534/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Week 8 before class: Diversity and inclusion",
+      "course": "CSE95_WI24_A00",
+      "dueDate": "2024-02-28T10:00:00",
+      "link": "https://www.gradescope.com/courses/682820/assignments/3801535/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Week 4 before class: Growth mindset, inclusion",
+      "course": "CSE95_WI24_A00",
+      "dueDate": "2024-01-31T08:00:00",
+      "link": "https://www.gradescope.com/courses/682820/assignments/3801536/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Week 6 before class: Professionalism and ethics of tutoring",
+      "course": "CSE95_WI24_A00",
+      "dueDate": "2024-02-14T10:00:00",
+      "link": "https://www.gradescope.com/courses/682820/assignments/3801537/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Week 7 before class: Academic integrity",
+      "course": "CSE95_WI24_A00",
+      "dueDate": "2024-02-21T10:00:00",
+      "link": "https://www.gradescope.com/courses/682820/assignments/3801543/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Week 9 before class: Learning from feedback and instructor-tutor interactions",
+      "course": "CSE95_WI24_A00",
+      "dueDate": "2024-03-06T10:00:00",
+      "link": "https://www.gradescope.com/courses/682820/assignments/3801545/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Midterm assignment: Peer Observation",
+      "course": "CSE95_WI24_A00",
+      "dueDate": "2024-02-09T17:00:00",
+      "link": "https://www.gradescope.com/courses/682820/assignments/3801921/submissions/new",
+      "submitted": false
+    }
+  ]
 };
