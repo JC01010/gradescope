be5bd64627c4da7cdb836f173f8ec6eb48fa9aa3
planit <planit@tanjeffreyz.github.io>
2024-01-18 01:37:35 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -4,7 +4,7 @@ assignments = {
       "title": "HW1",
       "course": "CSE101_WI24_A00",
       "dueDate": "2024-01-18T23:59:00",
-      "link": "https://www.gradescope.com/courses/693283/assignments/3925346/submissions/222721317",
+      "link": "https://www.gradescope.com/courses/693283/assignments/3925346/submissions/222929510",
       "submitted": true
     }
   ],
