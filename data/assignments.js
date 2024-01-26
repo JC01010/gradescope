8bec9275fca0336fbfcd5ef6ee8af82e6ccd9db5
planit <planit@tanjeffreyz.github.io>
2024-01-26 06:34:52 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -94,6 +94,13 @@ assignments = {
       "submitted": false
     },
     {
+      "title": "Week 4 Wednesday Review Quiz",
+      "course": "CSE105_WI24_A00",
+      "dueDate": "2024-02-02T17:00:00",
+      "link": "https://www.gradescope.com/courses/688881",
+      "submitted": false
+    },
+    {
       "title": "Week 3 Wednesday Review Quiz",
       "course": "CSE105_WI24_A00",
       "dueDate": "2024-01-26T19:00:00",
