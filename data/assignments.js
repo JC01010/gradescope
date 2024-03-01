6954eb7a758f2f63dbf42128a1e40992023bc7d3
planit <planit@tanjeffreyz.github.io>
2024-03-01 20:35:16 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -344,8 +344,8 @@ assignments = {
       "title": "Week 8 in-class group assignment",
       "course": "CSE95_WI24_A00",
       "dueDate": "2024-02-28T13:00:00",
-      "link": "https://www.gradescope.com/courses/682820/assignments/3801548/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/682820/assignments/3801548/submissions/234978026",
+      "submitted": true
     },
     {
       "title": "Midterm assignment: Peer Observation",
