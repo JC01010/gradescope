6945c882ef73861ea613bf814b5ee78d92082aa3
planit <planit@tanjeffreyz.github.io>
2024-03-31 18:34:32 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,3 +1,12 @@
 assignments = {
+  "MATH 183": [
+    {
+      "title": "HW 1-7",
+      "course": "MATH 183",
+      "dueDate": "2024-04-08T21:00:00",
+      "link": "https://www.gradescope.com/courses/755013",
+      "submitted": false
+    }
+  ],
   "MATH 187B - Spring 2024": []
 };
