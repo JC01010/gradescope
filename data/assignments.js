4fb9a8c6e8b2c67c76692bf8c2622439eff30ebd
planit <planit@tanjeffreyz.github.io>
2024-04-04 16:36:21 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,4 +1,35 @@
 assignments = {
+  "MAE131A_SP24_B00": [],
+  "MAE170_SP24_A00": [
+    {
+      "title": "LAB 1 TUESDAY AM (A01)",
+      "course": "MAE170_SP24_A00",
+      "dueDate": "2024-04-08T11:59:00",
+      "link": "https://www.gradescope.com/courses/761347/assignments/4303150/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "LAB 1 THURSDAY AM (A03)",
+      "course": "MAE170_SP24_A00",
+      "dueDate": "2024-04-10T11:59:00",
+      "link": "https://www.gradescope.com/courses/761347/assignments/4303222/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Lab 1 Wednesday AM (A02)",
+      "course": "MAE170_SP24_A00",
+      "dueDate": "2024-04-10T08:00:00",
+      "link": "https://www.gradescope.com/courses/761347",
+      "submitted": false
+    },
+    {
+      "title": "Lab 1 Wednesday PM (A05)",
+      "course": "MAE170_SP24_A00",
+      "dueDate": "2024-04-10T14:00:00",
+      "link": "https://www.gradescope.com/courses/761347",
+      "submitted": false
+    }
+  ],
   "MAE30B_SP24_B00": [],
   "MATH 183": [
     {
