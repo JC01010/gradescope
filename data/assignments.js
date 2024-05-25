20a96f62d06eb2db7cd6856a07f94ce20c2ad689
planit <planit@tanjeffreyz.github.io>
2024-05-25 05:33:54 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -168,7 +168,7 @@ assignments = {
     {
       "title": "Homework 5",
       "course": "MAE30B_SP24_B00",
-      "dueDate": "2024-05-24T23:59:00",
+      "dueDate": "2024-05-25T23:59:00",
       "link": "https://www.gradescope.com/courses/761713/assignments/4510052/submissions/255350188",
       "submitted": true
     }
