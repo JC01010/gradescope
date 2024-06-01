391ba9c11e2024a87fca5ee6c58ec575de62c5e9
planit <planit@tanjeffreyz.github.io>
2024-06-01 20:34:45 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -141,6 +141,13 @@ assignments = {
       "dueDate": "2024-06-05T23:59:00",
       "link": "https://www.gradescope.com/courses/761347/assignments/4497366/submissions/new",
       "submitted": false
+    },
+    {
+      "title": "Lab 8 - Calibration Thursday AM (A03)",
+      "course": "MAE170_SP24_A00",
+      "dueDate": "2024-06-12T23:59:00",
+      "link": "https://www.gradescope.com/courses/761347/assignments/4544620/submissions/new",
+      "submitted": false
     }
   ],
   "MAE30B_SP24_B00": [
