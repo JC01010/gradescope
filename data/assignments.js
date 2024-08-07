11d219f285a093802c31b87b0376731f23818376
planit <planit@tanjeffreyz.github.io>
2024-08-07 16:41:33 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,5 +1,13 @@
 assignments = {
-  "MAE107_S224_A00": [],
+  "MAE107_S224_A00": [
+    {
+      "title": "Homework 1 - Theory",
+      "course": "MAE107_S224_A00",
+      "dueDate": "2024-08-11T22:00:00",
+      "link": "https://www.gradescope.com/courses/814871",
+      "submitted": false
+    }
+  ],
   "MATH10B_S224_C00": [
     {
       "title": "Discussion 1",
