611077ec8fbca5afbd420abfe5cad43ce5b00389
planit <planit@tanjeffreyz.github.io>
2024-08-07 18:38:55 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -8,13 +8,5 @@ assignments = {
       "submitted": false
     }
   ],
-  "MATH10B_S224_C00": [
-    {
-      "title": "Discussion 1",
-      "course": "MATH10B_S224_C00",
-      "dueDate": "2024-08-06T23:59:00",
-      "link": "https://www.gradescope.com/courses/815763",
-      "submitted": false
-    }
-  ]
+  "MATH10B_S224_C00": []
 };
