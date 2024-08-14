209a7f8c31bfd22543f89bce48eae13ef2ac5f32
planit <planit@tanjeffreyz.github.io>
2024-08-14 06:40:14 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -6,6 +6,13 @@ assignments = {
       "dueDate": "2024-08-11T22:00:00",
       "link": "https://www.gradescope.com/courses/814871/assignments/4701778/submissions/261049726",
       "submitted": true
+    },
+    {
+      "title": "Homework 2 - Theory",
+      "course": "MAE107_S224_A00",
+      "dueDate": "2024-08-18T22:00:00",
+      "link": "https://www.gradescope.com/courses/814871",
+      "submitted": false
     }
   ]
 };
