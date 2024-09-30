24b1c54ea2e1b3ed84f716e121eac716d5846ff6
planit <planit@tanjeffreyz.github.io>
2024-09-30 23:36:05 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,5 +1,14 @@
 assignments = {
   "COGS118B_FA24_A00": [],
+  "MAE11_FA24_A00": [
+    {
+      "title": "HW1",
+      "course": "MAE11_FA24_A00",
+      "dueDate": "2024-10-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/885449",
+      "submitted": false
+    }
+  ],
   "MAE143A_FA24_A00": [
     {
       "title": "Homework 1",
