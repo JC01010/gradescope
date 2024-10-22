32fc1db9da1094b1ef493daa799e7eb0230c20b0
planit <planit@tanjeffreyz.github.io>
2024-10-22 11:35:15 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -108,8 +108,8 @@ assignments = {
       "title": "Homework 3",
       "course": "MATH100A_FA24_A00",
       "dueDate": "2024-10-22T22:00:00",
-      "link": "https://www.gradescope.com/courses/887603",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/887603/assignments/5184936/submissions/280623791",
+      "submitted": true
     }
   ]
 };
