f44864ceef78bc3a3d97ca239cdf54a4e954926f
planit <planit@tanjeffreyz.github.io>
2025-01-01 02:09:35 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -3,42 +3,42 @@ assignments = {
     {
       "title": "HW1 Written Part",
       "course": "COGS118B_FA24_A00",
-      "dueDate": "2024-10-11T08:00:00",
+      "dueDate": "2025-10-11T08:00:00",
       "link": "https://www.gradescope.com/courses/880945/assignments/5117838/submissions/276903944",
       "submitted": true
     },
     {
       "title": "HW2 Written Part",
       "course": "COGS118B_FA24_A00",
-      "dueDate": "2024-10-23T08:00:00",
+      "dueDate": "2025-10-23T08:00:00",
       "link": "https://www.gradescope.com/courses/880945/assignments/5127474/submissions/280620494",
       "submitted": true
     },
     {
       "title": "HW3W",
       "course": "COGS118B_FA24_A00",
-      "dueDate": "2024-11-08T08:00:00",
+      "dueDate": "2025-11-08T08:00:00",
       "link": "https://www.gradescope.com/courses/880945/assignments/5226624/submissions/286886085",
       "submitted": true
     },
     {
       "title": "Homework Resubmission",
       "course": "COGS118B_FA24_A00",
-      "dueDate": "2024-12-15T23:59:00",
+      "dueDate": "2025-12-15T23:59:00",
       "link": "https://www.gradescope.com/courses/880945/assignments/5406505/submissions/297603532",
       "submitted": true
     },
     {
       "title": "Project Submission",
       "course": "COGS118B_FA24_A00",
-      "dueDate": "2024-12-13T19:00:00",
+      "dueDate": "2025-12-13T19:00:00",
       "link": "https://www.gradescope.com/courses/880945/assignments/5406561/submissions/297438403",
       "submitted": true
     },
     {
       "title": "Individual Contribution Statement and 3 Peer Reviews",
       "course": "COGS118B_FA24_A00",
-      "dueDate": "2024-12-15T23:59:00",
+      "dueDate": "2025-12-15T23:59:00",
       "link": "https://www.gradescope.com/courses/880945/assignments/5406579/submissions/297603096",
       "submitted": true
     }
@@ -47,49 +47,49 @@ assignments = {
     {
       "title": "HW1",
       "course": "MAE11_FA24_A00",
-      "dueDate": "2024-10-06T23:59:00",
+      "dueDate": "2025-10-06T23:59:00",
       "link": "https://www.gradescope.com/courses/885449/assignments/5075630/submissions/275224787",
       "submitted": true
     },
     {
       "title": "HW2",
       "course": "MAE11_FA24_A00",
-      "dueDate": "2024-10-13T23:59:00",
+      "dueDate": "2025-10-13T23:59:00",
       "link": "https://www.gradescope.com/courses/885449/assignments/5148337/submissions/277640806",
       "submitted": true
     },
     {
       "title": "HW3",
       "course": "MAE11_FA24_A00",
-      "dueDate": "2024-10-22T23:59:00",
+      "dueDate": "2025-10-22T23:59:00",
       "link": "https://www.gradescope.com/courses/885449/assignments/5188796/submissions/280939190",
       "submitted": true
     },
     {
       "title": "HW4",
       "course": "MAE11_FA24_A00",
-      "dueDate": "2024-10-29T23:59:00",
+      "dueDate": "2025-10-29T23:59:00",
       "link": "https://www.gradescope.com/courses/885449/assignments/5236130/submissions/283506829",
       "submitted": true
     },
     {
       "title": "HW 5",
       "course": "MAE11_FA24_A00",
-      "dueDate": "2024-11-11T23:59:00",
+      "dueDate": "2025-11-11T23:59:00",
       "link": "https://www.gradescope.com/courses/885449/assignments/5301286/submissions/288083727",
       "submitted": true
     },
     {
       "title": "HW 6",
       "course": "MAE11_FA24_A00",
-      "dueDate": "2024-11-18T23:59:00",
+      "dueDate": "2025-11-18T23:59:00",
       "link": "https://www.gradescope.com/courses/885449/assignments/5319533/submissions/290491354",
       "submitted": true
     },
     {
       "title": "HW 7",
       "course": "MAE11_FA24_A00",
-      "dueDate": "2024-11-24T23:59:00",
+      "dueDate": "2025-11-24T23:59:00",
       "link": "https://www.gradescope.com/courses/885449/assignments/5370615/submissions/292570807",
       "submitted": true
     }
@@ -98,112 +98,112 @@ assignments = {
     {
       "title": "Homework 1",
       "course": "MAE143A_FA24_A00",
-      "dueDate": "2024-10-04T23:59:00",
+      "dueDate": "2025-10-04T23:59:00",
       "link": "https://www.gradescope.com/courses/879912/assignments/5065240/submissions/274832042",
       "submitted": true
     },
     {
       "title": "Homework 1 Resubmission",
       "course": "MAE143A_FA24_A00",
-      "dueDate": "2024-10-11T23:59:00",
+      "dueDate": "2025-10-11T23:59:00",
       "link": "https://www.gradescope.com/courses/879912/assignments/5110237/submissions/277243800",
       "submitted": true
     },
     {
       "title": "Homework 2",
       "course": "MAE143A_FA24_A00",
-      "dueDate": "2024-10-11T23:59:00",
+      "dueDate": "2025-10-11T23:59:00",
       "link": "https://www.gradescope.com/courses/879912/assignments/5110242/submissions/277269054",
       "submitted": true
     },
     {
       "title": "Homework 2 Resubmission",
       "course": "MAE143A_FA24_A00",
-      "dueDate": "2024-10-18T23:59:00",
+      "dueDate": "2025-10-18T23:59:00",
       "link": "https://www.gradescope.com/courses/879912/assignments/5151418/submissions/279604990",
       "submitted": true
     },
     {
       "title": "Homework 3",
       "course": "MAE143A_FA24_A00",
-      "dueDate": "2024-10-18T23:59:00",
+      "dueDate": "2025-10-18T23:59:00",
       "link": "https://www.gradescope.com/courses/879912/assignments/5151442/submissions/279632754",
       "submitted": true
     },
     {
       "title": "Homework 3 Resubmission",
       "course": "MAE143A_FA24_A00",
-      "dueDate": "2024-10-25T23:59:00",
+      "dueDate": "2025-10-25T23:59:00",
       "link": "https://www.gradescope.com/courses/879912/assignments/5189727/submissions/282352989",
       "submitted": true
     },
     {
       "title": "Homework 4",
       "course": "MAE143A_FA24_A00",
-      "dueDate": "2024-10-25T23:59:00",
+      "dueDate": "2025-10-25T23:59:00",
       "link": "https://www.gradescope.com/courses/879912/assignments/5189733/submissions/282352702",
       "submitted": true
     },
     {
       "title": "Homework 4 Resubmission",
       "course": "MAE143A_FA24_A00",
-      "dueDate": "2024-11-01T23:59:00",
+      "dueDate": "2025-11-01T23:59:00",
       "link": "https://www.gradescope.com/courses/879912/assignments/5227290/submissions/284769083",
       "submitted": true
     },
     {
       "title": "Homework 5",
       "course": "MAE143A_FA24_A00",
-      "dueDate": "2024-11-08T23:59:00",
+      "dueDate": "2025-11-08T23:59:00",
       "link": "https://www.gradescope.com/courses/879912/assignments/5260484/submissions/287247154",
       "submitted": true
     },
     {
       "title": "Midterm Resubmission",
       "course": "MAE143A_FA24_A00",
-      "dueDate": "2024-11-15T23:59:00",
+      "dueDate": "2025-11-15T23:59:00",
       "link": "https://www.gradescope.com/courses/879912/assignments/5275625/submissions/289740384",
       "submitted": true
     },
     {
       "title": "Homework 5 Resubmission",
       "course": "MAE143A_FA24_A00",
-      "dueDate": "2024-11-15T23:59:00",
+      "dueDate": "2025-11-15T23:59:00",
       "link": "https://www.gradescope.com/courses/879912/assignments/5300077/submissions/289740140",
       "submitted": true
     },
     {
       "title": "Homework 6",
       "course": "MAE143A_FA24_A00",
-      "dueDate": "2024-11-15T23:59:00",
+      "dueDate": "2025-11-15T23:59:00",
       "link": "https://www.gradescope.com/courses/879912/assignments/5300082/submissions/289740889",
       "submitted": true
     },
     {
       "title": "Homework 6 Resubmission",
       "course": "MAE143A_FA24_A00",
-      "dueDate": "2024-11-22T23:59:00",
+      "dueDate": "2025-11-22T23:59:00",
       "link": "https://www.gradescope.com/courses/879912/assignments/5336799/submissions/292083595",
       "submitted": true
     },
     {
       "title": "Homework 7",
       "course": "MAE143A_FA24_A00",
-      "dueDate": "2024-11-22T23:59:00",
+      "dueDate": "2025-11-22T23:59:00",
       "link": "https://www.gradescope.com/courses/879912/assignments/5336801/submissions/292082941",
       "submitted": true
     },
     {
       "title": "Homework 7 Resubmission",
       "course": "MAE143A_FA24_A00",
-      "dueDate": "2024-11-29T23:59:00",
+      "dueDate": "2025-11-29T23:59:00",
       "link": "https://www.gradescope.com/courses/879912/assignments/5371555/submissions/293527459",
       "submitted": true
     },
     {
       "title": "Homework 8",
       "course": "MAE143A_FA24_A00",
-      "dueDate": "2024-12-06T23:59:00",
+      "dueDate": "2025-12-06T23:59:00",
       "link": "https://www.gradescope.com/courses/879912/assignments/5392630/submissions/295488135",
       "submitted": true
     }
@@ -212,63 +212,63 @@ assignments = {
     {
       "title": "Homework 1",
       "course": "MATH100A_FA24_A00",
-      "dueDate": "2024-10-08T22:00:00",
+      "dueDate": "2025-10-08T22:00:00",
       "link": "https://www.gradescope.com/courses/887603/assignments/5090247/submissions/275640091",
       "submitted": true
     },
     {
       "title": "Homework 2",
       "course": "MATH100A_FA24_A00",
-      "dueDate": "2024-10-15T22:00:00",
+      "dueDate": "2025-10-15T22:00:00",
       "link": "https://www.gradescope.com/courses/887603/assignments/5130728/submissions/278401662",
       "submitted": true
     },
     {
       "title": "Homework 3",
       "course": "MATH100A_FA24_A00",
-      "dueDate": "2024-10-22T22:00:00",
+      "dueDate": "2025-10-22T22:00:00",
       "link": "https://www.gradescope.com/courses/887603/assignments/5184936/submissions/281002355",
       "submitted": true
     },
     {
       "title": "Homework 4",
       "course": "MATH100A_FA24_A00",
-      "dueDate": "2024-10-29T22:00:00",
+      "dueDate": "2025-10-29T22:00:00",
       "link": "https://www.gradescope.com/courses/887603/assignments/5208193/submissions/283498765",
       "submitted": true
     },
     {
       "title": "Homework 5",
       "course": "MATH100A_FA24_A00",
-      "dueDate": "2024-11-05T22:00:00",
+      "dueDate": "2025-11-05T22:00:00",
       "link": "https://www.gradescope.com/courses/887603/assignments/5268282/submissions/285983005",
       "submitted": true
     },
     {
       "title": "Homework 6",
       "course": "MATH100A_FA24_A00",
-      "dueDate": "2024-11-12T22:00:00",
+      "dueDate": "2025-11-12T22:00:00",
       "link": "https://www.gradescope.com/courses/887603/assignments/5279475/submissions/288463476",
       "submitted": true
     },
     {
       "title": "Homework 7",
       "course": "MATH100A_FA24_A00",
-      "dueDate": "2024-11-19T22:00:00",
+      "dueDate": "2025-11-19T22:00:00",
       "link": "https://www.gradescope.com/courses/887603/assignments/5317812/submissions/290976936",
       "submitted": true
     },
     {
       "title": "Homework 8",
       "course": "MATH100A_FA24_A00",
-      "dueDate": "2024-11-26T22:00:00",
+      "dueDate": "2025-11-26T22:00:00",
       "link": "https://www.gradescope.com/courses/887603/assignments/5378001/submissions/293191719",
       "submitted": true
     },
     {
       "title": "Homework 9",
       "course": "MATH100A_FA24_A00",
-      "dueDate": "2024-12-03T22:00:00",
+      "dueDate": "2025-12-03T22:00:00",
       "link": "https://www.gradescope.com/courses/887603/assignments/5387617/submissions/294405302",
       "submitted": true
     }
