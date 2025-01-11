42410108062b0ccda71d150b1fc0ba1ff8dfafcf
planit <planit@tanjeffreyz.github.io>
2025-01-11 16:39:14 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,52 +1,10 @@
 assignments = {
-  "MATH103B_WI25_A00": [
+  "MAE101B_WI25_A00": [
     {
       "title": "Homework 1",
-      "course": "MATH103B_WI25_A00",
-      "dueDate": "2025-01-14T23:59:00",
-      "link": "https://www.gradescope.com/courses/943441",
-      "submitted": false
-    },
-    {
-      "title": "Homework 2",
-      "course": "MATH103B_WI25_A00",
-      "dueDate": "2025-01-21T23:59:00",
-      "link": "https://www.gradescope.com/courses/943441",
-      "submitted": false
-    },
-    {
-      "title": "Homework 3",
-      "course": "MATH103B_WI25_A00",
-      "dueDate": "2025-01-28T23:59:00",
-      "link": "https://www.gradescope.com/courses/943441",
-      "submitted": false
-    },
-    {
-      "title": "Homework 4",
-      "course": "MATH103B_WI25_A00",
-      "dueDate": "2025-02-11T23:59:00",
-      "link": "https://www.gradescope.com/courses/943441",
-      "submitted": false
-    },
-    {
-      "title": "Homework 5",
-      "course": "MATH103B_WI25_A00",
-      "dueDate": "2025-02-18T23:59:00",
-      "link": "https://www.gradescope.com/courses/943441",
-      "submitted": false
-    },
-    {
-      "title": "Homework 6",
-      "course": "MATH103B_WI25_A00",
-      "dueDate": "2025-02-25T23:59:00",
-      "link": "https://www.gradescope.com/courses/943441",
-      "submitted": false
-    },
-    {
-      "title": "Homework 7",
-      "course": "MATH103B_WI25_A00",
-      "dueDate": "2025-03-11T23:59:00",
-      "link": "https://www.gradescope.com/courses/943441",
+      "course": "MAE101B_WI25_A00",
+      "dueDate": "2025-01-19T23:59:00",
+      "link": "https://www.gradescope.com/courses/949099",
       "submitted": false
     }
   ]
