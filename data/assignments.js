6fccb694db9b949927cf8021efa2da0ae7f7b0cb
planit <planit@tanjeffreyz.github.io>
2025-01-20 17:35:09 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -6,6 +6,13 @@ assignments = {
       "dueDate": "2025-01-19T23:59:00",
       "link": "https://www.gradescope.com/courses/949099/assignments/5576475/submissions/300700907",
       "submitted": true
+    },
+    {
+      "title": "Homework 2",
+      "course": "MAE101B_WI25_A00",
+      "dueDate": "2025-01-26T23:59:00",
+      "link": "https://www.gradescope.com/courses/949099",
+      "submitted": false
     }
   ]
 };
